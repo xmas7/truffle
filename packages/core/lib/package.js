556c774a7a781b54ce33967bb439d272ad538f6a
David Murdoch <david@davidmurdoch.com>
2021-10-29 15:11:34 -0400

    parse correctly

--- packages/core/lib/package.js
+++ packages/core/lib/package.js
@@ -7,7 +7,7 @@ const { createInterfaceAdapter } = require("@truffle/interface-adapter");
 const path = require("path");
 const fs = require("fs");
 const OS = require("os");
-const StreamingWeb3HttpProvider = require("@truffle/stream-provider");
+const StreamingWeb3HttpProvider = require("@truffle/stream-provider").default;
 
 const Package = {
   install: async function(options, callback) {
