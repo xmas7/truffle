ae1d84b3b4b8105dd5fbf5944d62909e436c949b
cds-amal <cds.sudama@gmail.com>
2022-07-19 02:50:59 -0400

    wip: mocha report

--- packages/compile-solidity/scripts/test.sh
+++ packages/compile-solidity/scripts/test.sh
@@ -5,8 +5,8 @@ set -o errexit
 yarn prepare
 
 if [ "$CI" = true ]; then
-  mocha ./test/** ./test/**/* --timeout 70000 $@
+  mocha  --reporter json --reporter-option output=../mocha-compile-solidity.json ./test/** ./test/**/* --timeout 70000 $@
 else
   rm -rf ./node_modules/.cache/truffle
-  mocha ./test/** ./test/**/* --invert --grep native --timeout 70000 $@
+  mocha --reporter json --reporter-option output=../mocha-compile-solidity.json ./test/** ./test/**/* --invert --grep native --timeout 70000 $@
 fi
--- packages/truffle/scripts/test.sh
+++ packages/truffle/scripts/test.sh
@@ -12,13 +12,13 @@ if ! yarn semver -r ">=12" $NODE_VERSION; then
 fi
 
 if [ "$GETH" == true ]; then
-  mocha --timeout 50000 --grep '@ganache|@standalone' --invert --colors $@
+  mocha --reporter json --reporter-option output=../mocha-truffle-geth.json --timeout 50000 --grep '@ganache|@standalone' --invert --colors $@
 elif [ "$FABRICEVM" == true ]; then
   mocha --timeout 50000 --grep @fabric-evm --colors $@
 elif [ "$COVERAGE" == true ]; then
   NO_BUILD=true mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 elif [ "$INTEGRATION" == true ]; then
-  mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
+  mocha --reporter json --reporter-option output=../mocha-truffle-integration.json --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 else
-  yarn build && mocha --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
+  yarn build && mocha --reporter json --reporter-option output=../mocha-truffle.json --no-warnings --timeout 20000 --grep $INVERSE_GREP --invert --colors $@
 fi
